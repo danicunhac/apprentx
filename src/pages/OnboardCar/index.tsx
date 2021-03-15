4b19c1ba64eb50348eb961169f5709e5f587f601
Daniel Cunha <danielcc.jp@gmail.com>
2021-03-14 23:44:12 -0300

    feat(done): onboardingcar page

--- src/pages/OnboardCar/index.tsx
+++ src/pages/OnboardCar/index.tsx
@@ -1,12 +1,45 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Image, StatusBar, View } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { useNavigation } from '@react-navigation/native';
+import OnboardingDot from '../../components/OnboardingDot';
 
-import { Container } from './styles';
+import car from '../../assets/car.png';
+import chevronRight from '../../assets/chevron-right.png';
+
+import { Container, Number, Title, Description, Footer, Dots } from './styles';
 
 const OnboardCar: React.FC = () => {
+  const navigation = useNavigation();
+
   return (
     <Container>
-      <Text>OnboardCar</Text>
+      <StatusBar barStyle="dark-content" backgroundColor="#fff" />
+      <View
+        style={{
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+        }}
+      >
+        <Image source={car} />
+        <Number>02</Number>
+      </View>
+      <View style={{ marginTop: 96 }}>
+        <Title>Depois, escolha o carro</Title>
+        <Description>
+          Você é quem define um período, e nós mostraremos os carros disponíveis
+        </Description>
+      </View>
+      <Footer>
+        <Dots>
+          <OnboardingDot />
+          <OnboardingDot isActive />
+        </Dots>
+        <TouchableOpacity onPress={() => navigation.navigate('OnboardWelcome')}>
+          <Image style={{ height: 24, width: 24 }} source={chevronRight} />
+        </TouchableOpacity>
+      </Footer>
     </Container>
   );
 };
--- src/pages/OnboardCar/styles.ts
+++ src/pages/OnboardCar/styles.ts
@@ -1,4 +1,43 @@
-import { View } from 'react-native';
+import { Text, View } from 'react-native';
 import styled from 'styled-components';
 
-export const Container = styled(View)``;
+export const Container = styled(View)`
+  background-color: #fff;
+  flex: 1;
+  padding: 160px 32px 34px 32px;
+`;
+
+export const Number = styled(Text)`
+  color: #ebebf0;
+  font-size: 64px;
+  font-family: 'Archivo-Bold';
+`;
+
+export const Title = styled(Text)`
+  font-size: 40px;
+  line-height: 42px;
+  width: 180px;
+  color: #47474d;
+  font-family: 'Archivo-Bold';
+`;
+
+export const Description = styled(Text)`
+  font-size: 15px;
+  line-height: 25px;
+  font-family: 'Inter-Regular';
+  margin-top: 24px;
+  width: 224px;
+`;
+
+export const Footer = styled(View)`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 96px;
+`;
+
+export const Dots = styled(View)`
+  flex-direction: row;
+  width: 16px;
+  justify-content: space-between;
+`;
